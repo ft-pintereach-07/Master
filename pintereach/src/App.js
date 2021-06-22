ceb07756c4b0b2b4ffb4f33b53165d3660da03e2
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-22 14:21:08 -0600

    basic idea for home page

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
+const Register = styled.button`
+  background-color: #161B22,
+  color: white,
+  align-items: center;
+`
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pintereach</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Pintereach</h1>
+        <Register className='register-btn'>Register</Register>
+        </header>
+      </div>
+    </Router>
   );
 }
 
