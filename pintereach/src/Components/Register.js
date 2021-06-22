b62103ff91898dac891fc4b12c4683a30f26ec0f
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-22 14:29:57 -0600

    new folder and file with frame for registration form

---
+++ pintereach/src/Components/Register.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Register() {
+    const initialValue = {
+        username: '',
+        password: '',
+        email: '',
+    }
+
+    return(
+        <div className='reg-form'>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default Register;
