c1df97a5a6a7e06af516b436a69e696f9f923fa5
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-23 15:08:50 -0600

    setting up article page

--- pintereach/src/Components/ArticleList.js
+++ pintereach/src/Components/ArticleList.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { axiosWithAuth } from '../Authorization/axiosAuth';
+
 
 class ArticleList extends React.Component {
     state = {
         articles: [],
     }
 
-    
+    getArticles = () => {
+        axiosWithAuth()
+        .get('')
+        .then(res => {
+            this.setState({...this.state, friends: [...res.data]
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    componentDidMound(){
+        this.getArticles()
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
 }
 
 export default ArticleList;
